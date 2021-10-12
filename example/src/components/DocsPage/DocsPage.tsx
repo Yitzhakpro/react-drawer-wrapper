7d346e24dd206a2dacb23df6566d97031816c203
Yitzhak <isako14@gmail.com>
2021-10-12 23:47:35 +0300

    added under construction on docs page

--- example/src/components/DocsPage/DocsPage.tsx
+++ example/src/components/DocsPage/DocsPage.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { WarningOutlined } from '@ant-design/icons';
 
 const DocsPage = () => {
-    return <h1>docs</h1>;
+    return (
+        <div
+            style={{
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <h1>Under Construction</h1>
+            <WarningOutlined
+                style={{
+                    fontSize: '16px',
+                    marginLeft: '5px',
+                    marginBottom: '5px',
+                    color: 'orange'
+                }}
+            />
+        </div>
+    );
 };
 
 export default DocsPage;
