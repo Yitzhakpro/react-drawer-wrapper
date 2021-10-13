12b38b6af86ba7d0c7e8fe037f5bb82a9c1580ce
Yitzhak <isako14@gmail.com>
2021-10-13 22:22:48 +0300

    Updates

---
+++ precache-manifest.f5f16bfd74df7897a7d7792b76b18d19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ee15ed5df381ac37c36a38d061efc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc9cf93cb16811375afa",
+    "url": "./static/css/2.486ebae0.chunk.css"
+  },
+  {
+    "revision": "dd4a4710317657bd4915",
+    "url": "./static/css/main.c682faf5.chunk.css"
+  },
+  {
+    "revision": "bc9cf93cb16811375afa",
+    "url": "./static/js/2.331137b0.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.331137b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd4a4710317657bd4915",
+    "url": "./static/js/main.d7a28f8d.chunk.js"
+  },
+  {
+    "revision": "d6135cd1cbe23a1240d2",
+    "url": "./static/js/runtime-main.bb9ad1f1.js"
+  },
+  {
+    "revision": "93a44c99b8863d951f806758bf34e780",
+    "url": "./static/media/react-logo.93a44c99.svg"
+  }
+]);
